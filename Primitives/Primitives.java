33883ed33c5dbf30bd36571adaaec4ba2302c04c
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-19 15:42:12 +0100

    add more types

--- Primitives/Primitives.java
+++ Primitives/Primitives.java
@@ -1,5 +1,6 @@
 public class Primitives {
     public static void main(String[] args) {
+        //integers
         int myMinInt = Integer.MIN_VALUE;
         int myMaxInt = Integer.MAX_VALUE;
         
@@ -12,7 +13,32 @@ public class Primitives {
         long minLong = Long.MIN_VALUE;
         long maxLong = Long.MAX_VALUE;
 
-        System.out.println(minLong);
-        System.out.println(maxLong);
+        //type casting
+        int anInt = 2;
+        byte castThing = (byte)(anInt / 2);
+
+        //float and double
+        float myFloat = 0.2f;
+        double myDouble = 0.2d;
+
+        System.out.println(myFloat * myDouble);
+        System.out.println(myDouble * myDouble);
+
+        System.out.println(convertType(2, true));
+        System.out.println(convertType(2, false));
+
+
+        //string, char and boolean
+        String myString = "something"; //not actually a primitive type
+        char myChar = 'a';
+        boolean myBool = true;
+
+    }
+
+    public static double convertType(int val, boolean poundInp) {
+        if (poundInp == true) {
+            return val * 0.45359237d;
+        }
+        return val / 0.45359237d;
     }
 }
