7803f05ffea0e298a4f0510fb38560d9eed9bc93
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-19 11:39:14 +0100

    add primitives project

---
+++ Primitives/Primitives.java
@@ -0,0 +1,18 @@
+public class Primitives {
+    public static void main(String[] args) {
+        int myMinInt = Integer.MIN_VALUE;
+        int myMaxInt = Integer.MAX_VALUE;
+        
+        byte minByte = Byte.MIN_VALUE;
+        byte maxByte = Byte.MAX_VALUE;
+
+        short minShort = Short.MIN_VALUE;
+        short maxShort = Short.MAX_VALUE;
+
+        long minLong = Long.MIN_VALUE;
+        long maxLong = Long.MAX_VALUE;
+
+        System.out.println(minLong);
+        System.out.println(maxLong);
+    }
+}
