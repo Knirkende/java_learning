9ba7492fa6cb0a37d5e8c58d978da9a8030f6496
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-20 17:11:44 +0100

    add more exercises

--- Flow/Exercise.java
+++ Flow/Exercise.java
@@ -33,4 +33,50 @@ public class Exercise {
     public static boolean hasTeen(int n1, int n2, int n3) {
         return checkRange(n1) || checkRange(n2) || checkRange(n3);
     }
+    //section 5 exercises
+    public static void printNumberInWord(int n) {
+        String r;
+        switch (n) {
+            case 0:
+                r = "ZERO";
+                break;
+            case 1:
+                r = "ONE";
+                break;
+            case 2:
+                r = "TWO";
+                break;
+            case 3:
+                r = "THREE";
+                break;
+            case 4:
+                r = "FOUR";
+                break;
+            case 5:
+                r = "FIVE";
+                break;
+            case 6:
+                r = "SIX";
+                break;
+            case 7:
+                r = "SEVEN";
+                break;
+            case 8:
+                r = "EIGHT";
+                break;
+            case 9:
+                r = "NINE";
+                break;
+            default:
+                r = "OTHER";
+        }
+        System.out.println(r);
+    }
+    public static void printNumberInWordAlt(int n) {
+        String s;
+        String[] a = {"ZERO", "ONE", "TWO", "THREE", "FOUR", "FIVE", "SIX", "SEVEN", "EIGHT", "NINE"};
+        if (n < 10 && n >= 0) s = a[n];
+        else s = "OTHER";
+        System.out.println(s);
+    }
 }
