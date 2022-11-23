b6e5f2cc1ceebf75f2e2e5e265ab1ca9a0249b25
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-23 17:59:18 +0100

    basic numeric types parsing

---
+++ inputParsing/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String numStr = "2018";
+        int num = Integer.parseInt(numStr);
+        System.out.println(num + 1);
+        double numDouble = Double.parseDouble(numStr);
+        System.out.println(numDouble);
+    }
+}
