6a242643e50d64546c8e03c27a15148798b05ab1
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-24 09:54:09 +0100

    add an exercise

--- inputParsing/Exercises.java
+++ inputParsing/Exercises.java
@@ -1,6 +1,9 @@
+import java.util.Scanner;
+
 public class Exercises {
     public static void main(String[] args) {
-        System.out.println(canPack(2, 1, 5));
+        //System.out.println(canPack(2, 1, 5));
+        inputThenPrintSumAndAverage();
     }
 
     public static boolean canPack (int bigCount, int smallCount, int goal) {
@@ -8,5 +11,16 @@ public class Exercises {
         if (bigCount * 5 <= goal) {
             return smallCount >= (goal - bigCount * 5);
         } return smallCount >= goal % 5;
+    }
+    public static void inputThenPrintSumAndAverage() {
+        Scanner scanner = new Scanner(System.in);
+        int total = 0;
+        int counter = 0;
+        while (scanner.hasNextInt()) {
+            total += scanner.nextInt();
+            counter += 1;
+        }
+        scanner.close();
+        System.out.println("SUM = " + total + " AVG = " + Math.round((double)total / counter));
     }
 }
