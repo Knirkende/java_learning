8e4631592208770684a0384883b230a9369c8f18
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-24 09:14:52 +0100

    Scanner class

--- inputParsing/Main.java
+++ inputParsing/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         String numStr = "2018";
@@ -5,5 +7,24 @@ public class Main {
         System.out.println(num + 1);
         double numDouble = Double.parseDouble(numStr);
         System.out.println(numDouble);
+
+        // Scanner
+        int numNum = -1;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter stuff: ");
+        String name = scanner.nextLine();
+
+        System.out.println("Enter num: ");
+        
+        if ( scanner.hasNextInt() ) {
+            numNum = scanner.nextInt();
+        } else {
+            System.out.println("Good job. Great.");
+        }
+        
+        scanner.close();
+        System.out.println("Stuff was " + name);
+        System.out.println("Num was " + numNum);
+
     }
 }
