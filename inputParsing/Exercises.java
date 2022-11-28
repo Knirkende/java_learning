1b1194de8de4457f6afe93cffab73f0c9f545055
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-28 12:32:07 +0100

    more exercises

--- inputParsing/Exercises.java
+++ inputParsing/Exercises.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Exercises {
     public static void main(String[] args) {
         //System.out.println(canPack(2, 1, 5));
-        inputThenPrintSumAndAverage();
+        //inputThenPrintSumAndAverage();
+        System.out.println(getBucketCount(3.4,2.1,1.5,2));
     }
 
     public static boolean canPack (int bigCount, int smallCount, int goal) {
@@ -22,5 +23,23 @@ public class Exercises {
         }
         scanner.close();
         System.out.println("SUM = " + total + " AVG = " + Math.round((double)total / counter));
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) return -1;
+        int result = (int)(Math.ceil((width * height) / areaPerBucket)) - extraBuckets;
+        result = result > 0 ? result : 0;
+        return result;
+    }
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) return -1;
+        int result = (int)(Math.ceil((width * height) / areaPerBucket));
+        result = result > 0 ? result : 0;
+        return result;
+    }
+    public static int getBucketCount(double area, double areaPerBucket) {
+        if (area <= 0 || areaPerBucket <= 0) return -1;
+        int result = (int)(Math.ceil((area) / areaPerBucket));
+        result = result > 0 ? result : 0;
+        return result;
     }
 }
