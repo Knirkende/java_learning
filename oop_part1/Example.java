ed8e9322a24e949cb9fcd3766bd45f91cd828372
Ole Holgernes <ole.holgernes@gmail.com>
2022-11-28 12:46:48 +0100

    oop initial commit

---
+++ oop_part1/Example.java
@@ -0,0 +1,6 @@
+public class Example {
+    public int beep; // accessible everywhere
+    protected int boop; // accessible to this package and subclasses elsewhere
+    int meep; // package access
+    private int moop; // only accessible by this class
+}
