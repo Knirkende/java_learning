0e04df4056d2298d7ab76d07b61aea7c3457f983
Ole Holgernes <ole.holgernes@gmail.com>
2022-12-01 17:45:35 +0100

    add getters and setters

--- oop_part1/Main.java
+++ oop_part1/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Example ex = new Example();
+        System.out.println(ex.getMoop());
+        ex.setMoop(5);
+        System.out.println(ex.getMoop());
     }
 }
