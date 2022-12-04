d67319fa1de757d16016a6a67b2d0cbb17db1aba
Ole Holgernes <ole.holgernes@gmail.com>
2022-12-04 10:30:47 +0100

    constructor chaining

--- oop_part1/Example.java
+++ oop_part1/Example.java
@@ -3,7 +3,7 @@ public class Example {
     protected int boop; // accessible to this package and subclasses elsewhere
     int meep = 3; // package access
     private int moop = 4; // only accessible by this class
-    
+
     public static boolean top = true; // static, common for all instances
 
     public Example(int beep, int boop) { // this is a constructor
@@ -11,11 +11,16 @@ public class Example {
         this.boop = boop;
     }
 
+    public Example() { // constructor chaining
+        this(0, 0);
+    }
+
     public int getMoop() {
         return moop;
     }
+
     public void setMoop(int moop) {
         this.moop = moop;
     }
-    
+
 }
--- oop_part1/Main.java
+++ oop_part1/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        Example ex = new Example(5, 6);
+        Example ex = new Example();
         System.out.println(ex.beep);
     }
 }
