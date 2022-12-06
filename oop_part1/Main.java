b44ed9d6216869f51ae6574f33ab4cee665e3c49
Ole Holgernes <ole.holgernes@gmail.com>
2022-12-06 18:07:52 +0100

    add POJO example code

--- oop_part1/Main.java
+++ oop_part1/Main.java
@@ -2,5 +2,19 @@ public class Main {
     public static void main(String[] args) {
         Example ex = new Example();
         System.out.println(ex.beep);
+        for (int i = 1; i <= 5; i++) {
+            StudentPojo s = new StudentPojo(
+                "123" + i,
+                switch (i) {
+                    case 1 -> "one";
+                    case 2 -> "two";
+                    case 3 -> "three";
+                    case 4 -> "four";
+                    case 5 -> "five";
+                    default -> "nothing";
+                },
+                "5445",
+                "blabla");
+        }
     }
 }
---
+++ oop_part1/StudentPojo.java
@@ -0,0 +1,14 @@
+public class StudentPojo {
+    private String id;
+    private String name;
+    private String dateOfBirth;
+    private String classList;
+    
+    public StudentPojo(String id, String name, String dateOfBirth, String classList) {
+        this.id = id;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.classList = classList;
+    }
+    
+}
