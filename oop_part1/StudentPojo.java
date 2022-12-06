a4c0945eedbe8c47189f2fc0a9618b8966ae7512
Ole Holgernes <ole.holgernes@gmail.com>
2022-12-06 18:30:33 +0100

    add toString method

--- oop_part1/StudentPojo.java
+++ oop_part1/StudentPojo.java
@@ -3,12 +3,18 @@ public class StudentPojo {
     private String name;
     private String dateOfBirth;
     private String classList;
-    
+
     public StudentPojo(String id, String name, String dateOfBirth, String classList) {
         this.id = id;
         this.name = name;
         this.dateOfBirth = dateOfBirth;
         this.classList = classList;
     }
-    
+
+    @Override
+    public String toString() {
+        return "StudentPojo [id=" + id + ", name=" + name + ", dateOfBirth=" + dateOfBirth + ", classList=" + classList
+                + "]";
+    }
+
 }
