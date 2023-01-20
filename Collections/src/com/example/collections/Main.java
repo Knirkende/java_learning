78826d05430c7b3d4e0b7ed024082c9e37bbcc75
Ole Holgernes <ole.holgernes@gmail.com>
2023-01-20 09:17:32 +0100

    add Collections list methods

--- Collections/src/com/example/collections/Main.java
+++ Collections/src/com/example/collections/Main.java
@@ -1,11 +1,12 @@
 package com.example.collections;
 
+import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
         Theatre theatre = new Theatre("Place", 8, 12);
         //theatre.getSeats();
-
+/*
         if (theatre.reserveSeat("H11")) {
             System.out.println("Okay");
         } else {
@@ -16,6 +17,51 @@ public class Main {
         } else {
             System.out.println("No");
         }
+*/
+        List<Theatre.Seat> seatCopy = new ArrayList<>(theatre.seats); //shallow copy
+        printList(seatCopy);
+        // demonstrates shallow copy
+        seatCopy.get(1).reserve();
+        if (theatre.reserveSeat("A02")) {
+            System.out.println("Done, works.");
+        } else {
+            System.out.println("Already reserved, not done.");
+        }
+        Collections.shuffle(seatCopy);
+        System.out.println("seatCopy:");
+        printList(seatCopy);
+        System.out.println("theatre.seats:");
+        printList(theatre.seats);
+        // min and max
+        Theatre.Seat minSeat = Collections.min(seatCopy);
+        Theatre.Seat maxSeat = Collections.max(seatCopy);
 
+        sortList(seatCopy);
+        System.out.println();
+        System.out.println("Bubblesorted seatCopy:");
+        printList(seatCopy);
+
+    }
+    public static void printList(List<Theatre.Seat> list) {
+        for(Theatre.Seat seat : list) {
+            System.out.print(" " + seat.getSeatNumber());
+        }
+        System.out.println();
+        System.out.println("-----");
+    }
+
+    // bubble sort
+    public static void sortList(List<? extends Theatre.Seat> list) {
+        int n = 0;
+        for (int i = 0; i < list.size() - 1; i++) {
+            for(int j = i+1; j < list.size(); j++) {
+                n++;
+                if(list.get(i).compareTo(list.get(j)) > 0) {
+                    Collections.swap(list, i, j);
+                }
+            }
+        }
+        System.out.println();
+        System.out.println("Bubblesort n = " + n);
     }
 }
--- Collections/src/com/example/collections/Theatre.java
+++ Collections/src/com/example/collections/Theatre.java
@@ -5,7 +5,7 @@ import java.util.*;
 public class Theatre {
 
     private final String theatreName;
-    private List<Seat> seats = new ArrayList<>();
+    public List<Seat> seats = new ArrayList<>();
 
     public String getTheatreName() {
         return theatreName;
@@ -52,7 +52,7 @@ public class Theatre {
         }
     }
 
-    private class Seat implements Comparable<Seat> {
+    public class Seat implements Comparable<Seat> {
         private final String seatNumber;
         private boolean reserved = false;
 
